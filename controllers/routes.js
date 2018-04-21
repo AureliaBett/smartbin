0757d5ddc4c8d306889b4af1ace6709411d8d78d
Jack Stringer <jackstringer@gmail.com>
2018-04-21 12:17:13 -0700

    Testing post routed

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,11 +3,13 @@ var router = express.Router();
 var db = require('../models/db');
 
 router.get('/', function(req, res) {
+  res.send('hello there');
   //get database data
+  //render to view
 });
 
 router.post('/trash', function(req, res){
-  
+  console.log(req.body);
 });
 
 module.exports = router;
